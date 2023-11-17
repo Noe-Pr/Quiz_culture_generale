9b5a281793feb60409b7ea78b686db054bcc973e
Noé <132721280+Noe-Pr@users.noreply.github.com>
2023-11-17 09:27:37 -0400

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,226 @@
+// let questions = [
+//     {
+//         numb: 1,
+//         question: "Qu’est-ce qui est considéré comme le poumon de la Terre ?",
+//         answer: "A. La forêt amazonienne",
+//         options: [
+//             "A. La forêt amazonienne",
+//             "B. Le fleuve Amazone",
+//             "C. Le Sahara",
+//             "D. Le lac Baïkal"
+
+//         ]
+//     },
+//     {
+//         numb: 2,
+//         question: "Dans quelle ville se trouve l'université de Harvard ?",
+//         answer: "B. Boston",
+//         options: [
+//             "A. New York",
+//             "B. Boston",
+//             "C. Washington D.C.",
+//             "D. Chicago"
+//         ]
+//     },
+//     {
+//         numb: 3,
+//         question: "Quel empereur romain a construit le célèbre Colisée de Rome ?",
+//         answer: "D. Vespasien",
+//         options: [
+//             "A. Jules César",
+//             "B. Caligula",
+//             "C. Néron",
+//             "D. Vespasien"
+//         ]
+//     },
+//     {
+//         numb: 4,
+//         question: "Quel est le plus grand pays d'Amérique du Sud ?",
+//         answer: "A. Brésil",
+//         options: [
+//             "A. Brésil",
+//             "B. Argentine",
+//             "C. Pérou",
+//             "D. Colombie"
+//         ]
+//     },
+//     {
+//         numb: 5,
+//         question: "Quelle est la capitale de l'Australie?",
+//         answer: "C. Canberra",
+//         options: [
+//             "A. Sydney",
+//             "B. Melbourne",
+//             "C. Canberra",
+//             "D. Brisbane"
+//         ]
+//     },
+//     {
+//         numb: 6,
+//         question: "Quelle est l'altitude du Mont Everest ?",
+//         answer: "C. 8 848 m",
+//         options: [
+//             "A. 6 895 m",
+//             "B. 10 994 m",
+//             "C. 8 848 m",
+//             "D. 9 409 m"
+//         ]
+//     },
+//     {
+//         numb: 7,
+//         question: "Quelle est la plus grande religion du monde en termes de nombre de fidèles ?",
+//         answer: "A. Christianisme",
+//         options: [
+//             "A. Christianisme",
+//             "B. Islam",
+//             "C. Hindouisme",
+//             "D. Bouddhisme"
+//         ]
+//     },
+//     {
+//         numb: 8,
+//         question: "Qui a écrit le roman 'L'Étranger' ?",
+//         answer: "A. Albert Camus",
+//         options: [
+//             "A. Albert Camus",
+//             "B. Jean-Paul Sartre",
+//             "C. Marcel Proust",
+//             "D. Gustave Flaubert"
+//         ]
+//     },
+//     {
+//         numb: 9,
+//         question: "Quelle est la planète la plus proche du soleil ?",
+//         answer: "D. Mercure",
+//         options: [
+//             "A. Vénus",
+//             "B. Uranus",
+//             "C. Terre",
+//             "D. Mercure"
+//         ]
+//     },
+//     {
+//         numb: 10,
+//         question: "Quel est le pays d'origine du peintre Pablo Picasso ?",
+//         answer: "A. Espagne",
+//         options: [
+//             "A. Espagne",
+//             "B. France",
+//             "C. Italie",
+//             "D. Pays-Bas"
+//         ]
+//     },
+// ];
+
+let questions = [
+    {
+        numb: 1,
+        question: "Quel est le plus grand désert du monde ?",
+        answer: "B. Le désert de l'Antarctique",
+        options: [
+            "A. Le Sahara",
+            "B. Le désert de l'Antarctique",
+            "C. Le désert de Gobi",
+            "D. Le désert d'Atacama"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Quelle est la plus grande île du monde ?",
+        answer: "C. Groenland",
+        options: [
+            "A. Madagascar",
+            "B. Bornéo",
+            "C. Groenland",
+            "D. Nouvelle-Guinée"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Qui a écrit 'Roméo et Juliette' ?",
+        answer: "A. William Shakespeare",
+        options: [
+            "A. William Shakespeare",
+            "B. Charles Dickens",
+            "C. Jane Austen",
+            "D. Mark Twain"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Dans quelle année a eu lieu la Révolution française ?",
+        answer: "D. 1789",
+        options: [
+            "A. 1776",
+            "B. 1804",
+            "C. 1812",
+            "D. 1789"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Quel est le symbole chimique de l'oxygène ?",
+        answer: "B. O",
+        options: [
+            "A. Ox",
+            "B. O",
+            "C. Oxg",
+            "D. Oxy"
+        ]
+    },
+    {
+        numb: 6,
+        question: "Qui a peint 'La Nuit étoilée' ?",
+        answer: "A. Vincent van Gogh",
+        options: [
+            "A. Vincent van Gogh",
+            "B. Pablo Picasso",
+            "C. Leonardo da Vinci",
+            "D. Claude Monet"
+        ]
+    },
+    {
+        numb: 7,
+        question: "Quel est le plus long fleuve du monde ?",
+        answer: "C. Le Nil",
+        options: [
+            "A. L'Amazone",
+            "B. Le Mississippi",
+            "C. Le Nil",
+            "D. Le Yangtsé"
+        ]
+    },
+    {
+        numb: 8,
+        question: "Qui a découvert la pénicilline ?",
+        answer: "B. Alexander Fleming",
+        options: [
+            "A. Louis Pasteur",
+            "B. Alexander Fleming",
+            "C. Marie Curie",
+            "D. Albert Einstein"
+        ]
+    },
+    {
+        numb: 9,
+        question: "Quel est le pays le plus peuplé du monde ?",
+        answer: "D. La Chine",
+        options: [
+            "A. L'Inde",
+            "B. Les États-Unis",
+            "C. L'Indonésie",
+            "D. La Chine"
+        ]
+    },
+    {
+        numb: 10,
+        question: "Qui a écrit '1984' ?",
+        answer: "C. George Orwell",
+        options: [
+            "A. Aldous Huxley",
+            "B. F. Scott Fitzgerald",
+            "C. George Orwell",
+            "D. Ernest Hemingway"
+        ]
+    },
+];
